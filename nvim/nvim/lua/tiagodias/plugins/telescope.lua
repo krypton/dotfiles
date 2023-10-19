4d91aa138d23f024179bb02c6210720612f4dd23
Tiago Dias <tiago.dias@cldware.com>
2023-10-19 16:04:21 +0100

    Allow search on hidden files and dotfiles o telescope live grep

--- nvim/nvim/lua/tiagodias/plugins/telescope.lua
+++ nvim/nvim/lua/tiagodias/plugins/telescope.lua
@@ -16,9 +16,20 @@ return {
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 		local builtin = require("telescope.builtin")
+		local telescopeConfig = require("telescope.config")
+
+		-- Clone the default Telescope configuration
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, "!**/.git/*")
 
 		telescope.setup({
 			defaults = {
+				vimgrep_arguments = vimgrep_arguments,
 				file_ignore_patterns = { "%.git/" },
 				path_display = { "truncate " },
 				mappings = {
