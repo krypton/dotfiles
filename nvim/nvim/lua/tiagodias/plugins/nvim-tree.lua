a8561df7c8c82a6dc6f36d6cf08833414f1ed02e
Tiago Dias <tiago.dias@cldware.com>
2023-11-15 16:33:20 +0000

    nvim-tree alignments

--- nvim/nvim/lua/tiagodias/plugins/nvim-tree.lua
+++ nvim/nvim/lua/tiagodias/plugins/nvim-tree.lua
@@ -1,55 +1,55 @@
 return {
-   "nvim-tree/nvim-tree.lua",
-   version = "*",
-   dependencies = {
-      "nvim-tree/nvim-web-devicons",
-   },
-   config = function()
-      local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-      nvimtree.setup({
-         on_attach = function(bufnr)
-            local api = require('nvim-tree.api')
+		nvimtree.setup({
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
 
-            local function opts(desc)
-               return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-            end
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
 
-            -- OR use all default mappings
-            api.config.mappings.default_on_attach(bufnr)
+				-- OR use all default mappings
+				api.config.mappings.default_on_attach(bufnr)
 
-            -- add your mappings
-            vim.keymap.set("n", "v", api.node.open.vertical, opts "Open: Vertical Split")
-            vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close Directory")
-            vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-            vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-         end,
-         sync_root_with_cwd = false,
-         respect_buf_cwd = false,
-         live_filter = {
-            prefix = "[FILTER]: ",
-            always_show_folders = false, -- Turn into false from true by default
-         },
-         update_focused_file = {
-            enable = true,
-         },
-         diagnostics = {
-            enable = true,
-            show_on_dirs = true,
-            icons = {
-               hint = "",
-               info = "",
-               warning = "",
-               error = "",
-            },
-         },
-         view = {
-            width = {
-               min = 30,
-               max = -1,
-            },
-            side = "left",
-         },
-      })
-   end
+				-- add your mappings
+				vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+				vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+				vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
+				vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+			end,
+			sync_root_with_cwd = false,
+			respect_buf_cwd = false,
+			live_filter = {
+				prefix = "[FILTER]: ",
+				always_show_folders = false, -- Turn into false from true by default
+			},
+			update_focused_file = {
+				enable = true,
+			},
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+				icons = {
+					hint = "",
+					info = "",
+					warning = "",
+					error = "",
+				},
+			},
+			view = {
+				width = {
+					min = 30,
+					max = -1,
+				},
+				side = "left",
+			},
+		})
+	end,
 }
