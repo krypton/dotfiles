fda90f85d93a57ce7640dce2e286b78983a9d39d
Tiago Dias <tiago.dias@cldware.com>
2023-11-30 17:08:04 +0000

    bufferline config changes

--- nvim/nvim/lua/tiagodias/plugins/bufferline.lua
+++ nvim/nvim/lua/tiagodias/plugins/bufferline.lua
@@ -2,17 +2,34 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	options = {
-		mode = "buffers",
-		numbers = "ordinal",
-		truncate_names = false, -- This allows us to open buffers with the same filename
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = " File Explorer",
-				highlight = "Directory",
-				separator = false,
+	event = "VeryLazy",
+	opts = {
+		options = {
+			mode = "buffers",
+			numbers = "ordinal",
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = false,
+			truncate_names = false,
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					highlight = "Directory",
+					text_align = "left",
+					separator = false,
+				},
 			},
 		},
 	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+		-- Fix bufferline when restoring a session
+		vim.api.nvim_create_autocmd("BufAdd", {
+			callback = function()
+				vim.schedule(function()
+					pcall(nvim_bufferline)
+				end)
+			end,
+		})
+	end,
 }
