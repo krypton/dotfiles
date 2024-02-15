3470346805b1daee250934e2bed9e9ca86d190ea
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-02-15 11:53:59 +0000

    Feature: best plugin integration that I have between vim-tpipeline and tmux rose pine statusline

--- .config/nvim/lua/tiagodias/plugins/tmux.lua
+++ .config/nvim/lua/tiagodias/plugins/tmux.lua
@@ -11,8 +11,10 @@ return {
 	{
 		"vimpostor/vim-tpipeline",
 		config = function()
+			-- Disable the statusline when entering vim
+			vim.g.tpipeline_autoembed = 0
 			-- Restore the previous statusline after quitting vim
-			vim.g.tpipeline_restore = 1
+			-- vim.g.tpipeline_restore = 1
 			-- Clear duplicate statusline when there is split windows
 			vim.g.tpipeline_clearstl = 1
 		end,
