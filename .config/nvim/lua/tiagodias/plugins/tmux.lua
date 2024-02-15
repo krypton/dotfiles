5869198b7ac59a7e357cdb69bcd4fe980f6487f2
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-02-15 11:24:42 +0000

    Integrate vim statusline into tmux statusline

--- .config/nvim/lua/tiagodias/plugins/tmux.lua
+++ .config/nvim/lua/tiagodias/plugins/tmux.lua
@@ -1,10 +1,20 @@
 -- This integrates with the same package used on TPM package manager on .tmux.conf
 return {
-    "aserowy/tmux.nvim",
-    config = function()
-        local tmux = require("tmux")
+	{
+		"aserowy/tmux.nvim",
+		config = function()
+			local tmux = require("tmux")
 
-        tmux.setup()
-    end
+			tmux.setup()
+		end,
+	},
+	{
+		"vimpostor/vim-tpipeline",
+		config = function()
+			-- Restore the previous statusline after quitting vim
+			vim.g.tpipeline_restore = 1
+			-- Clear duplicate statusline when there is split windows
+			vim.g.tpipeline_clearstl = 1
+		end,
+	},
 }
-
