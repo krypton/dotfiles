2fa3dba5b656cd8b22767b156ab7887f25cf8c39
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-03-12 16:33:26 +0000

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+local config = {
+	-- https://wezfurlong.org/wezterm/config/lua/config/term.html
+	term = "wezterm",
+	font = wezterm.font("JetBrainsMonoNL Nerd Font Propo"),
+	font_size = 13.0,
+	color_scheme = "rose-pine-moon",
+	window_background_opacity = 0.95,
+	text_background_opacity = 0.95,
+	enable_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = false,
+	window_close_confirmation = "NeverPrompt",
+}
+
+return config
