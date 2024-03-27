03da24d12387b10dbf8c5abb97291a5d67a48761
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-03-27 11:16:03 +0000

    Add neogit plugin

---
+++ .config/nvim/lua/tiagodias/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
+}
