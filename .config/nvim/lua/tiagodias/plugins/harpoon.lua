459af2a809003eb030a4032c8eccaebc77357427
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-03-27 10:53:13 +0000

    Harpoon 2

---
+++ .config/nvim/lua/tiagodias/plugins/harpoon.lua
@@ -0,0 +1,30 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		local extensions = require("harpoon.extensions")
+
+		-- REQUIRED
+		harpoon:setup()
+		-- REQUIRED
+
+		harpoon:extend(extensions.builtins.navigate_with_number())
+
+		vim.keymap.set("n", "<leader>ah", function()
+			harpoon:list():append()
+		end)
+		vim.keymap.set("n", "<leader>th", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<leader>ph", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<leader>nh", function()
+			harpoon:list():next()
+		end)
+	end,
+}
