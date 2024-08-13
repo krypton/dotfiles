61d6e4a45fae6c2190238c8bda84d8620980b692
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-08-13 15:31:25 +0100

    Use wezterm config_builder to set the options and maximize window on startup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,20 @@
 local wezterm = require("wezterm")
-local config = {
-	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" }),
-	font_size = 12.0,
-	color_scheme = "Tokyo Night Moon",
-	window_background_opacity = 0.95,
-	text_background_opacity = 0.95,
-	enable_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
-}
+local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+config.font_size = 12.0
+config.color_scheme = "Tokyo Night Moon"
+config.window_background_opacity = 0.97
+config.text_background_opacity = 0.97
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
+
+-- Maximize window on startup
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 return config
