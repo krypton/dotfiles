d19aee7c1490f2c9d7dae0ada09298c0651e07f6
Tiago Dias <tiagofrederico.dias@gmail.com>
2024-08-13 12:41:34 +0100

    Wezterm window decorations

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require("wezterm")
 local config = {
-	-- https://wezfurlong.org/wezterm/config/lua/config/term.html
-	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" }),
 	font_size = 12.0,
 	color_scheme = "Tokyo Night Moon",
 	window_background_opacity = 0.95,
@@ -9,12 +8,7 @@ local config = {
 	enable_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = false,
 	window_close_confirmation = "NeverPrompt",
-	window_padding = {
-		left = 2,
-		right = 2,
-		top = 2,
-		bottom = 2,
-	},
+	window_decorations = "RESIZE",
 }
 
 return config
