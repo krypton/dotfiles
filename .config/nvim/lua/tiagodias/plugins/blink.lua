155c9fae4720146bcc73f845dd08d9865ca3f270
Tiago Dias <tiago.dias@binhuman.com>
2024-12-18 10:10:08 +0000

    copilot on blink.cmp

--- .config/nvim/lua/tiagodias/plugins/blink.lua
+++ .config/nvim/lua/tiagodias/plugins/blink.lua
@@ -15,7 +15,40 @@ return {
 		},
 		appearance = {
 			use_nvim_cmp_as_default = true,
-			nerd_font_variant = 'mono'
+			nerd_font_variant = 'mono',
+			kind_icons = {
+				Copilot = "",
+				Text = '󰉿',
+				Method = '󰊕',
+				Function = '󰊕',
+				Constructor = '󰒓',
+
+				Field = '󰜢',
+				Variable = '󰆦',
+				Property = '󰖷',
+
+				Class = '󱡠',
+				Interface = '󱡠',
+				Struct = '󱡠',
+				Module = '󰅩',
+
+				Unit = '󰪚',
+				Value = '󰦨',
+				Enum = '󰦨',
+				EnumMember = '󰦨',
+
+				Keyword = '󰻾',
+				Constant = '󰏿',
+
+				Snippet = '󱄽',
+				Color = '󰏘',
+				File = '󰈔',
+				Reference = '󰬲',
+				Folder = '󰉋',
+				Event = '󱐋',
+				Operator = '󰪚',
+				TypeParameter = '󰬛',
+			},
 		},
 		snippets = {
 			expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
@@ -35,7 +68,19 @@ return {
 					module = "blink-cmp-copilot",
 					score_offset = 100,
 					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
 				},
+			},
+			completion = {
+				enabled_providers = { 'lsp', 'path', 'luasnip', 'snippets', 'buffer', 'copilot' },
 			},
 		},
 		signature = {
