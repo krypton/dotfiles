8080c2645623b2ac52c4720edec08aa162fc7e04
Tiago Dias <tiago.dias@binhuman.com>
2025-01-08 10:38:48 +0000

    fix: breaking change with luasnip and blink

--- .config/nvim/lua/tiagodias/plugins/blink.lua
+++ .config/nvim/lua/tiagodias/plugins/blink.lua
@@ -59,18 +59,9 @@ local M = {
 					TypeParameter = '󰬛',
 				},
 			},
-			snippets = {
-				expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-				active = function(filter)
-					if filter and filter.direction then
-						return require('luasnip').jumpable(filter.direction)
-					end
-					return require('luasnip').in_snippet()
-				end,
-				jump = function(direction) require('luasnip').jump(direction) end,
-			},
+			snippets = { preset = 'luasnip' },
 			sources = {
-				default = { 'lsp', 'path', 'luasnip', 'snippets', 'buffer', 'copilot', 'lazydev' },
+				default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot', 'lazydev' },
 				providers = {
 					copilot = {
 						name = "copilot",
