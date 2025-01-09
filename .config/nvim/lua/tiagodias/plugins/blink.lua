cbb8de67942c08b1ebc43a836c60f161060c338d
Tiago Dias <tiago.dias@binhuman.com>
2025-01-09 10:32:27 +0000

    better blink.cmp config

--- .config/nvim/lua/tiagodias/plugins/blink.lua
+++ .config/nvim/lua/tiagodias/plugins/blink.lua
@@ -11,62 +11,84 @@ local M = {
 		},
 	},
 	{
-		'saghen/blink.cmp',
-		lazy = false,
+		"saghen/blink.cmp",
 		dependencies = {
-			{ 'rafamadriz/friendly-snippets' },
+			{ "rafamadriz/friendly-snippets" },
 			{ "giuxtaposition/blink-cmp-copilot" },
-			{ 'L3MON4D3/LuaSnip',                version = 'v2.*' },
+			{ "L3MON4D3/LuaSnip",                version = "v2.*" },
 			{ "folke/lazydev.nvim", },
 		},
-		version = 'v0.*',
 		opts = {
-			keymap = { preset = 'default' },
-			appearance = {
-				use_nvim_cmp_as_default = false,
-				nerd_font_variant = 'mono',
-				kind_icons = {
-					Copilot = "",
-					Text = '󰉿',
-					Method = '󰊕',
-					Function = '󰊕',
-					Constructor = '󰒓',
-
-					Field = '󰜢',
-					Variable = '󰆦',
-					Property = '󰖷',
-
-					Class = '󱡠',
-					Interface = '󱡠',
-					Struct = '󱡠',
-					Module = '󰅩',
-
-					Unit = '󰪚',
-					Value = '󰦨',
-					Enum = '󰦨',
-					EnumMember = '󰦨',
-
-					Keyword = '󰻾',
-					Constant = '󰏿',
-
-					Snippet = '󱄽',
-					Color = '󰏘',
-					File = '󰈔',
-					Reference = '󰬲',
-					Folder = '󰉋',
-					Event = '󱐋',
-					Operator = '󰪚',
-					TypeParameter = '󰬛',
-				},
+			keymap = { preset = "default" },
+			snippets = {
+				preset = "luasnip",
+				-- This comes from the luasnip extra, if you don"t add it, won"t be able to
+				-- jump forward or backward in luasnip snippets
+				-- https://www.lazyvim.org/extras/coding/luasnip#blinkcmp-optional
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
 			},
-			snippets = { preset = 'luasnip' },
 			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot', 'lazydev' },
+				default = { "lsp", "path", "snippets", "buffer", "copilot", "lazydev" },
 				providers = {
+					lsp = {
+						name = "lsp",
+						enabled = true,
+						module = "blink.cmp.sources.lsp",
+						-- When linking markdown notes, I would get snippets and text in the
+						-- suggestions, I want those to show only if there are no LSP
+						-- suggestions
+						-- Disabling fallbacks as my snippets wouldn't show up
+						-- Enabled fallbacks as this seems to be working now
+						fallbacks = { "lazydev", "snippets", "buffer" },
+						score_offset = 90, -- the higher the number, the higher the priority
+					},
+					path = {
+						name = "Path",
+						module = "blink.cmp.sources.path",
+						score_offset = 25,
+						-- When typing a path, I would get snippets and text in the
+						-- suggestions, I want those to show only if there are no path
+						-- suggestions
+						fallbacks = { "snippets", "buffer" },
+						opts = {
+							trailing_slash = false,
+							label_trailing_slash = true,
+							get_cwd = function(context)
+								return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+							end,
+							show_hidden_files_by_default = true,
+						},
+					},
+					buffer = {
+						name = "Buffer",
+						enabled = true,
+						max_items = 3,
+						module = "blink.cmp.sources.buffer",
+						score_offset = 15, -- the higher the number, the higher the priority
+					},
+					snippets = {
+						name = "snippets",
+						enabled = true,
+						max_items = 8,
+						module = "blink.cmp.sources.snippets",
+						score_offset = 85, -- the higher the number, the higher the priority
+					},
 					copilot = {
 						name = "copilot",
+						enabled = true,
 						module = "blink-cmp-copilot",
-						score_offset = 100,
+						score_offset = -100, -- the higher the number, the higher the priority
 						async = true,
 						transform_items = function(_, items)
 							local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
@@ -78,20 +100,34 @@ local M = {
 							return items
 						end,
 					},
-					-- dont show LuaLS require statements when lazydev has items
-					lsp = { fallbacks = { "lazydev" } },
-					lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100, },
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
 				},
+				-- command line completion, thanks to dpetka2001 in reddit
+				-- https://www.reddit.com/r/neovim/comments/1hjjf21/comment/m37fe4d/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+				cmdline = function()
+					local type = vim.fn.getcmdtype()
+					if type == "/" or type == "?" then
+						return { "buffer" }
+					end
+					if type == ":" then
+						return { "cmdline" }
+					end
+					return {}
+				end,
 			},
 			signature = {
 				enabled = true,
-				window = { border = 'rounded' },
+				window = { border = "rounded" },
 			},
 			completion = {
-				menu = { border = 'rounded' },
+				menu = { border = "rounded" },
 				documentation = {
 					auto_show = true,
-					window = { border = 'rounded' },
+					window = { border = "rounded" },
 				},
 				ghost_text = {
 					enabled = true,
