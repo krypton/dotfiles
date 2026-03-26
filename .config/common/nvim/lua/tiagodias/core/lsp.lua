d6b19c01c5d6f9b98b3c56b9d4cc50c85676bc18
Tiago Dias <tiago.dias@binhuman.com>
2026-03-26 17:11:55 +0000

    chore(lsp): optimize what is loaded on lua_ls workspace

--- .config/common/nvim/lua/tiagodias/core/lsp.lua
+++ .config/common/nvim/lua/tiagodias/core/lsp.lua
@@ -6,7 +6,10 @@ vim.lsp.config("lua_ls", {
 				globals = { "vim", },
 			},
 			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = {
+					vim.env.VIMRUNTIME,
+					"${3rd}/luv/library",
+				},
 				checkThirdParty = false,
 			},
 			telemetry = { enable = false },
